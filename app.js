0317586fb28a71d1dd625c7921bb07fce2c12811
StepanChewbacca <qweqwe322322322@gmail.com>
2021-11-24 00:07:14 +0200

    todolist

--- app.js
+++ app.js
@@ -1,169 +1,75 @@
-const listOfPosts2 = [
-  {
-    id: 1,
-    post: "some post1",
-    title: "title1",
-    author: "Ivanov",
-    comments: [
-      {
-        id: 1.1,
-        comment: "some comment1",
-        title: "title1",
-        author: "Rimus",
-      },
-      {
-        id: 1.2,
-        comment: "some comment2",
-        title: "title2",
-        author: "Uncle",
-      },
-    ],
-  },
-  {
-    id: 2,
-    post: "some post2",
-    author: "Ivanov",
-    comments: [
-      {
-        id: 2.1,
-        comment: "some comment1",
-        title: "title1",
-        author: "Rimus",
-      },
-      {
-        id: 2.2,
-        comment: "some comment2",
-        title: "title2",
-        author: "Uncle",
-      },
-      {
-        id: 2.3,
-        comment: "some comment3",
-        title: "title3",
-        author: "Rimus",
-      },
-    ],
-  },
-  {
-    id: 3,
-    post: "some post3",
-    title: "title3",
-    author: "Rimus",
-  },
-  {
-    id: 4,
-    post: "some post4",
-    title: "title4",
-    author: "Uncle",
-  },
-];
+const input = document.querySelector('#input');
+const button = document.querySelector('.add');
+const result = document.querySelector('.result');
+const clear = document.querySelector('.clear');
+const remove = document.querySelector('.remove_option');
+let toDos  = [];
 
-//1
-function tickets(person) {
-  let index = 0;
-  let money = 0;
-  while (true) {
-    if (index === person.length) {
-      return true;
+button.addEventListener('click', ()  => {
+    toDos.push(input.value)
+    updateUiList(toDos)
+    saveToDosToLocalStorage(toDos)
+});
+
+remove.addEventListener('click', markAsDone);
+result.addEventListener('dblclick', removeFromList);
+result.addEventListener('auxclick', markAsDone);
+result.addEventListener('input', ifInput);
+clear.addEventListener('click', clearStorage);
+
+
+function updateUiList(toDos) {
+    result.innerHTML = ""
+    for (let i =0; i<toDos.length; i++) {
+        result.innerHTML += `<li contenteditable="true">${toDos[i]}</li>`;
     }
-    if (person[index] === 25) {
-      money += person[index];
-      index++;
-    } else {
-      if (person[index] > 25 && person[index] - 25 - money > 0) {
-        return false;
-      } else {
-        money += person[index] - 25;
-        index++;
-      }
-    }
-  }
+    return  result.innerHTML
 }
 
-console.log(tickets([25, 25, 50]));
-
-//2
-
-function getSum(firstNum, secondNum) {
-  const firstArr = firstNum.split("");
-  const secondArr = secondNum.split("");
-  const templateArr = [];
-  let finalArr = [];
-  if (firstArr.length > secondArr.length) {
-    while (secondArr.length !== firstArr.length) {
-      secondArr.unshift("0");
-    }
-  } else if (secondArr.length > firstArr.length) {
-    while (firstArr.length !== secondArr.length) {
-      firstArr.unshift("0");
-    }
-  }
-  for (let i = 0; i < firstArr.length; i++) {
-    if (+firstArr[i] + +secondArr[i] >= 10) {
-      templateArr.push(String(+firstArr[i] + +secondArr[i])[0]);
-      templateArr.push(String(+firstArr[i] + +secondArr[i])[1]);
-    } else {
-      templateArr.push("0");
-      templateArr.push(String(+firstArr[i] + +secondArr[i]));
-    }
-  }
-  templateArr.push("0");
-  if (templateArr[0] !== "0") {
-    finalArr.push(templateArr[0]);
-    finalArr.push(templateArr[1]);
-    templateArr[1] = "0";
-  }
-  for (let i = 1; i < templateArr.length; i++) {
-    finalArr.push(String(+templateArr[i] + +templateArr[i + 1]));
-  }
-  finalArr = finalArr.filter((item, index) => index % 2 === 0);
-  return String(finalArr.join(""));
+function getToDosFromLocalStorage() {
+    return  JSON.parse(localStorage.getItem('toDos'))
 }
 
-const firstNumber = "11111111111111111111";
-const secondNumber = "233333333333333333333333333333333";
-console.log(getSum(firstNumber, secondNumber));
-
-//3
-
-function getQuantityPostsByAuthor(listOfPosts, author) {
-  let posts = 0;
-  let comments = 0;
-
-  for (let data of listOfPosts) {
-    if (data.author === author) {
-      posts++;
-    }
-    if (Object.keys(data).includes("comments")) {
-      data.comments.forEach((element) => {
-        if (element.author === author) {
-          comments++;
-        }
-      });
-    }
-  }
-
-  return [posts, comments];
+function saveToDosToLocalStorage (toDos) {
+    localStorage.setItem('toDos', JSON.stringify(toDos));
 }
 
-console.log(getQuantityPostsByAuthor(listOfPosts2, "Rimus"));
-
-//4
-
-const complexFunction = (arg1, arg2) => arg1 + arg2;
-
-function cache() {
-  const arr = [];
-  return function () {
-    if (arr.includes(arguments[0])) {
-      return arr;
-    }
-    arr.push(arguments[0]);
-    return arr;
-  };
+function clearStorage () {
+    localStorage.removeItem('toDos');
+    updateUiList(toDos)
 }
 
-const cachedFunc = cache();
-console.log(cachedFunc(complexFunction("foo", "bar")));
-console.log(cachedFunc(complexFunction("foo", "bar")));
-console.log(cachedFunc(complexFunction("foo", "baz")));
+function ifInput () {
+    saveToDosToLocalStorage();
+}
+
+function markAsDone (e) {
+    if (e.target.id !== 'Done') {
+        e.target.setAttribute('id', 'Done');
+        e.target.style = 'text-decoration: line-through';
+        saveToDosToLocalStorage();
+        return
+    }
+    if (e.target.id === 'Done') {
+        e.target.setAttribute('id', 'Not_Done');
+        e.target.style = '';
+        saveToDosToLocalStorage();
+        return
+    }
+}
+
+function removeFromList (e) {
+    if (remove.id !== 'Done') {
+        const arrayIndex  =  toDos.findIndex(elem => elem === e.target.innerText)
+        toDos.splice(arrayIndex-1,1)
+        updateUiList(toDos)
+        saveToDosToLocalStorage(toDos)
+    }
+}
+
+
+
+if (localStorage.getItem('toDos')) {
+    toDos = getToDosFromLocalStorage()
+    updateUiList(toDos)
+}
--- test.spec.js
+++ test.spec.js
@@ -1,137 +1,43 @@
-describe('tickets', () => {
-    it('first person > 25', () => {
-        expect(tickets([100, 25, 50])).to.false
-    })
-    it('clear array', () => {
-        expect(tickets([])).to.true
-    })
-    it('not enough money ', () => {
-        expect(tickets([25,100,25])).to.false
-    })
-    it('enough money', () => {
-        expect(tickets([25,25,50])).to.true
+describe('updateUiList', () => {
+         const toDos = ["1", "1", "1", "1"]
+         updateUiList(toDos)
+    it('toDos length =  list.children.length', () => {
+        let list = document.querySelector('.result')
+        expect(list.children.length).to.equal(toDos.length);
     })
 })
 
-describe('getSum', () => {
-    it('second number is bigger then first number', () => {
-        expect(getSum("2333333333333313333333333333333333","111111112111111111111")).to.equal('2333333333333424444445444444444444')
+
+describe('Local Storage', () => {
+    const toDos = ["1", "1", "1", "1"]
+    it('clearStorage', () => {
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+        clearStorage()
+        expect(localStorage.getItem('toDos')).to.equal(null);
     })
-    it('first number is bigger then second number', () => {
-        expect(getSum("111111112111111111111" ,"2333333333333313333333333333333333")).to.equal('2333333333333424444445444444444444')
+    it('AddToStorage', () => {
+        const toDosStringify = JSON.stringify(toDos);
+        localStorage.setItem('toDos', toDosStringify);
+        expect(localStorage.getItem('toDos')).to.equal(toDosStringify);
     })
 })
 
-describe('getQuantityPostsByAuthor', () => {
-    const listOfPosts1 = [
-        {
-            id: 3,
-            post: "some post3",
-            title: "title3",
-            author: "Rimus",
+
+describe('removeFromList', () => {
+    const toDos = ["12", "1", "1", "1"]
+    it('removeItem', () => {
+        let obj = {
+            target: {
+                innerText : 12
+            }
         }
-    ];
-    const listOfPosts3 = [
-        {
-            id: 3,
-            post: "some post3",
-            title: "title3",
-            author: "Timus",
-        }
-    ];
-    const listOfPosts2 = [
-        {
-            id: 1,
-            post: "some post1",
-            title: "title1",
-            author: "Ivanov",
-            comments: [
-                {
-                    id: 1.1,
-                    comment: "some comment1",
-                    title: "title1",
-                    author: "Rimus",
-                },
-                {
-                    id: 1.2,
-                    comment: "some comment2",
-                    title: "title2",
-                    author: "Uncle",
-                },
-            ],
-        },
-        {
-            id: 2,
-            post: "some post2",
-            author: "Ivanov",
-            comments: [
-                {
-                    id: 2.1,
-                    comment: "some comment1",
-                    title: "title1",
-                    author: "Rimus",
-                },
-                {
-                    id: 2.2,
-                    comment: "some comment2",
-                    title: "title2",
-                    author: "Uncle",
-                },
-                {
-                    id: 2.3,
-                    comment: "some comment3",
-                    title: "title3",
-                    author: "Rimus",
-                },
-            ],
-        },
-        {
-            id: 3,
-            post: "some post3",
-            title: "title3",
-            author: "Rimus",
-        },
-        {
-            id: 4,
-            post: "some post4",
-            title: "title4",
-            author: "Uncle",
-        },
-    ];
-    it('list without comments', () => {
-        expect(getQuantityPostsByAuthor(listOfPosts1,"Rimus" )).to.eql([ 1, 0 ])
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+        removeFromList (obj)
+        const toDosStringify = JSON.stringify(toDos);
+        expect(localStorage.getItem('toDos')).to.equal(toDosStringify);
+
     })
-    it('list without comments and posts', () => {
-        expect(getQuantityPostsByAuthor(listOfPosts3,"Rimus")).to.eql([ 0, 0 ])
-    })
-    it('random list ', () => {
-        expect(getQuantityPostsByAuthor(listOfPosts2,"Rimus")).to.eql([ 1, 3 ])
+    it('', () => {
+
     })
 })
-
-
-describe('cache', () => {
-    const cachedFunc = cache();
-
-    it('do not be in cache', () => {
-        expect(cachedFunc("foobartest")).to.eql(["foobartest"])
-    })
-    it('already in cache', () => {
-        cachedFunc("foobartest")
-        expect(cachedFunc("foobartest")).to.eql(["foobartest"])
-    })
-})
-
-
-describe('complexFunction', () => {
-    it('random arg', () => {
-        expect(complexFunction(1,2)).to.equal(3)
-    })
-    it('random arg', () => {
-        expect(complexFunction("foo", "bar")).to.eql("foobar")
-    })
-})
-
-
-
-
